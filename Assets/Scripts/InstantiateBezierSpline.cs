c8cc8e1983638b05af451fca0dc45e0361edf14d
Farrech <rrosalie@ensc.fr>
2018-01-22 12:33:16 +0100

    maj

--- Assets/Scripts/InstantiateBezierSpline.cs
+++ Assets/Scripts/InstantiateBezierSpline.cs
@@ -5,23 +5,23 @@ using UnityEngine;
 public class InstantiateBezierSpline : MonoBehaviour {
 
     public int radius;
-    private BezierSpline bez;
+    private BezierSpline bezierSpline;
 
 	void Awake () {
         float length = radius * 0.552284749f;
-        bez = this.GetComponent<BezierSpline>();
-        bez.Loop = false;
+        bezierSpline = this.GetComponent<BezierSpline>();
+        bezierSpline.Loop = false;
         Vector3 coord1 = new Vector3(-radius, 0, 0);
         Vector3 coord2 = new Vector3(0,0,radius);
         Vector3 coord3 = new Vector3(radius, 0, 0);
-        bez.SetControlPoint(0, coord1);
-        bez.SetControlPoint(1, TangenteOfPoint(coord1, length));
-        bez.SetControlPoint(3, coord2);
-        bez.SetControlPoint(2, TangenteOfPoint(coord2, -length));
-        bez.AddCurve();
-        bez.SetControlPoint(4, TangenteOfPoint(coord2, length));
-        bez.SetControlPoint(6, coord3);
-        bez.SetControlPoint(5, TangenteOfPoint(coord3, length));
+        bezierSpline.SetControlPoint(0, coord1);
+        bezierSpline.SetControlPoint(1, TangenteOfPoint(coord1, length));
+        bezierSpline.SetControlPoint(3, coord2);
+        bezierSpline.SetControlPoint(2, TangenteOfPoint(coord2, -length));
+        bezierSpline.AddCurve();
+        bezierSpline.SetControlPoint(4, TangenteOfPoint(coord2, length));
+        bezierSpline.SetControlPoint(6, coord3);
+        bezierSpline.SetControlPoint(5, TangenteOfPoint(coord3, length));
 	}
 	
 
